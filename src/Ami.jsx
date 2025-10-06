0e24cc6f21ca0cc92c24fd621b999a237a70aef9
Sarthak4321 <roysarthak16@gmail.com>
2025-10-06 23:00:58 +0530

    fixing

--- src/Ami.jsx
+++ src/Ami.jsx
@@ -147,7 +147,7 @@ export default function PopupPage({ isOpen, onClose, password }) {
     }
 
     try {
-      const res = await fetch("/send", {
+      const res = await fetch("https://generate-password-pi.vercel.app/send", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
